b4fd94dfeb4033a8ed0b61eef5bf79670655508d
calvo_r <calvo.robert.29217@gmail.com>
2015-11-11 20:44:27 +0100

    rajout ddu makefile sur linux

---
+++ Linux/Structure.h
@@ -0,0 +1,18 @@
+#ifndef STRUCTURE_H_INCLUDED
+#define STRUCTURE_H_INCLUDED
+
+
+struct difference   {
+
+	char diffType ;
+
+	int startLineSrc ;
+	int endLineSrc ;
+    int startLineDest ;
+	int endLineDest ;
+
+	t_difference* nextDiff;
+
+} t_difference;
+
+#endif // STRUCTURE_H_INCLUDED
---
+++ Linux/main.c
@@ -0,0 +1,49 @@
+/*
+    SCARPELLINI Xavier
+    CALVO Robert
+    MEZELLE Clément
+
+    Projet de langage C
+*/
+
+
+
+#include "Prototypes.h"
+
+
+
+int main(int argc, char** argv)
+{
+    int cursor = 1; // index of the current argument
+
+    if (!ArgumentsControl (argc, argv)) /** /!\ la deuxieme partie de la fonction démarre à partir du 3eme arguments au lieu de s'arreter 2 argument avant la fin **/
+        return 0;
+/*
+    FILE* fileSrc = ChargeFile(argv[1]);           // pointeur sur le premier fichier, source de la comparaison
+    FILE* fileCmp = ChargeFile(argv[2]);           // pointeur sur le second fichier, comparatif du fichier source
+
+    if (fileSrc == NULL  ||  fileCmp == NULL )
+        return 0;
+*/
+    void *fonctionList[] = {}; /** ajouter le nom de la fonction à l'index correspondant dans le tableau des options **/
+
+    while(cursor < argc-2){
+        if(argv[cursor][0] == '-'){
+            fonctionList[CheckOption(argv[cursor])](argv[1], argv[2], argv[cursor], argv[cursor+1], argv[cursor+2]); /** le nombre d'argument de cette fonction peut changer **/
+
+            /** creer un appel direct à la focntion via le tableau de pointeur de fonction + verifie le nombre d'argument à envoyer**/
+        }
+        cursor++;
+    }
+
+    char* lineSrc, *lineDest;
+
+
+    ////
+    ////
+
+
+//    FreeFiles(fileSrc, fileCmp);
+
+    return 0;
+}
