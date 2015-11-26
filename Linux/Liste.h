7c9540a82febd182d394588ea8d9470e25a9a674
krrom <krrom@live.fr>
2015-11-26 12:06:36 +0100

    suppression des commentaires inutiles + ajout de commentaires en anglais

--- Linux/Liste.h
+++
@@ -1,9 +0,0 @@
-#ifndef __LISTE_H__
-#define __LISTE_H__
-
-#include "Structure.h"
-
-t_difference* Add_list(t_difference* diff);
-void Delete_list(t_difference* diff);
-
-#endif
--- Linux/find_difference.h
+++
@@ -1,9 +0,0 @@
-#ifndef	__FIND_DIFFERENCE_H__
-#define	__FIND_DIFFERENCE_H__
-
-#include "Liste.h"
-
-int	find_difference(t_difference* diff, t_env* env);
-
-#endif
-
